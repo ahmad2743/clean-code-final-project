9a3a625f2c5b00d8b26e9069c3f84b08b2100060
ahmad2743 <amethdione10@gmail.com>
2022-02-26 22:00:31 +0100

    dictionnaire done

--- src/main/java/NumberRepresentation.java
+++ src/main/java/NumberRepresentation.java
@@ -1,4 +1,38 @@
+import javax.naming.directory.InvalidAttributesException;
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
 public class NumberRepresentation {
-    int[][] myMatrice = new int[3][3];
+
+    private int[][] myMatrix;
+    private int value;
+    NumberRepresentation(int[][] matrix){
+        if (matrix.length == 3)
+            this.myMatrix = matrix;
+        throw new InvalidParameterException();
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NumberRepresentation that = (NumberRepresentation) o;
+        return Arrays.equals(myMatrix, that.myMatrix);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(myMatrix);
+    }
 
 }
